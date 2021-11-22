85008567c3e710496ca5822b3be649a069bd8f04
David Lloyd <dlloyd32@gmail.com>
2021-11-21 22:39:25 -0500

    added options placeholder page

--- options.py
+++ options.py
@@ -2,13 +2,16 @@
 import template
 import streamlit as st
 
+
 class Options(template.Page):
+	"""Options page"""
+
+	def _single_symbol(self, symbol: str):
+		st.markdown('''
+		Eventually 
+		[Options3D](https://github.com/BrononymousEngineer/options3d) will be 
+		migrated to this page.
+		''')
 
 	def _multi_symbols(self, symbols: list):
-		st.markdown('''
-		Eventually [Options3D](https://options3d.herokuapp.com/) will be
-		migrated to this page.
-		
-		The github is 
-		[here](https://github.com/BrononymousEngineer/options3d).
-		''')
+		self._single_symbol(symbols[0] if symbols else symbols)
